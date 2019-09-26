a8ddf09bfeafdbfda45f6f2ba47321bb34a4885f
gatta <your.email@example.com>
2019-09-26 21:52:47 +0200

    upd

--- src/Handlers/Form/CommentsFormHandler.php
+++ src/Handlers/Form/CommentsFormHandler.php
@@ -19,6 +19,12 @@ class CommentsFormHandler
 
     public function handle(FormInterface $form)
     {
-        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $form = $form->getData();
+            $this->entityManager->persist($form);
+            $this->entityManager->flush();
+            
+            return true;
+        }
     }
 }
