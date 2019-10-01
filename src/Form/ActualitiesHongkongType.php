9a1c05be4fd7ba3c7ce86a00b81d44cd31560b2d
gatta <your.email@example.com>
2019-10-01 14:01:46 +0200

    upd

--- src/Form/ActualitiesHongkongType.php
+++ src/Form/ActualitiesHongkongType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\ActualitiesHongKong;
+use App\Entity\ActualitiesHongkong;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
